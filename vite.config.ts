4efee141e3e1ce6a5558931722f5fe44f4f15998
msankar1012 <41480318+msankar1012@users.noreply.github.com>
2025-12-29 21:44:56 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: [
-      "localhost",
-      ".csb.app",
-      "xz6t6s-5173.csb.app", // YOUR NEW SANDBOX DOMAIN
-    ],
-  },
-});
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+export default defineConfig({ plugins: [react()] });
